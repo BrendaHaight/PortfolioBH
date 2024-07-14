74dd9dce8d9bedbbfe78f6e672732eb06a3d117e
Brenda Haight <brendaaureliano9@gmail.com>
2024-07-14 15:53:55 -0500

    Creating componets and page files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <a
+      href="https://github.com/brendaHaight"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      GitHub
+    </a>
+    <a
+      href="www.linkedin.com/in/brenda-haight-b3a88014a"
+      target="_blank"
+      rel="noopener noreferrer"
+    ></a>
+    <a ref="" target="_blank" rel="noopener noreferrer"></a>
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/Project.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Project = ({ title, image, deployedLink, repoLink }) => (
+  <div className="project">
+    <h3>{title}</h3>
+    <img src="{image} alt={`${title} screenshot`}" />
+    <div>
+      <a href={deployedLink} target="_blank" rel="noopner noreferrer">
+        View App
+      </a>
+      <a href={repoLink} target="_blank" rel="noopener noreferrer">
+        GitHub Repo
+      </a>
+    </div>
+  </div>
+);
+
+export default Project;
