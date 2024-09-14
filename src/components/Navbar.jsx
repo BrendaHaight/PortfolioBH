cd6756ea30bd6ed55175bdd20f5ba2c22dd886d9
Brenda Haight <brendaaureliano9@gmail.com>
2024-09-13 20:28:33 -0500

    giving motion to the webpage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,9 +10,30 @@ const Navbar = () => {
         <img className="mx-4 w-10" src={logo} alt="logo"></img>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaInstagram />
+        <a
+          href="https://www.linkedin.com/in/brenda-haight-b3a88014a/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/BrendaHaight"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.instagram.com/brendaaureliano/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Instagram"
+        >
+          <FaInstagram />
+        </a>
       </div>
     </nav>
   );
